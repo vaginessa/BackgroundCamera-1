f46212c217880f1d9a15286f590921c0747502f6
Bikram Pandit <bikcrum@gmail.com>
2017-11-01 20:09:11 +0545

    untitled

--- app/src/main/java/com/nepdeveloper/backgroundcamera/broadcastReceiver/BootCompleted.java
+++ app/src/main/java/com/nepdeveloper/backgroundcamera/broadcastReceiver/BootCompleted.java
@@ -5,14 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.nepdeveloper.backgroundcamera.service.MyService;
+import com.nepdeveloper.backgroundcamera.utility.Constant;
 
 public class BootCompleted extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent serviceIntent = new Intent(context, MyService.class);
-            context.startService(serviceIntent);
+            if (context.getSharedPreferences(Constant.PREFERENCE_NAME, Context.MODE_PRIVATE)
+                    .getBoolean(Constant.SERVICE_ACTIVE, true)) {
+                Intent serviceIntent = new Intent(context, MyService.class);
+                context.startService(serviceIntent);
+            }
         }
     }
 }
